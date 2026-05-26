81c902002868ca4ee1d81046fcbafe5f36ab4f6c
Lyubov9302 <lyubovyurynets@gmail.com>
2026-05-26 11:42:30 +0300

    added examples of operators

--- src/app/components/angular/angular/angular.ts
+++ src/app/components/angular/angular/angular.ts
@@ -5,12 +5,17 @@ import { QUESTIONS } from '../../../constants';
 import {
   AsyncSubject,
   BehaviorSubject,
+  delay,
+  filter,
+  forkJoin,
   from,
+  map,
   Observable,
   of,
   ReplaySubject,
   Subject,
   Subscriber,
+  tap,
 } from 'rxjs';
 
 @Component({
@@ -22,21 +27,53 @@ import {
 })
 export class Angular implements OnInit {
   // Створення Subject
-  subjectFirst = new Subject<number>();
+  // subjectFirst = new Subject<number>();
 
   // Отримання Observable з Subject
-  observable = this.subjectFirst.asObservable();
+  // observable = this.subjectFirst.asObservable();
 
   // Cтворення BehaviorSubject з початковим значенням
   behaviorSubject = new BehaviorSubject<number>(0);
 
   // Cтворення ReplaySubject, який зберігає два останні значення
-  replaySubject = new ReplaySubject<number>(2);
+  // replaySubject = new ReplaySubject<number>(2);
 
   // Cтворення AsyncSuject
-  asyncSubject = new AsyncSubject<number>();
+  // asyncSubject = new AsyncSubject<number>();
+
+  // ***********  Створення декількох Observable, які моделюють асинхронні запити
+  observable1 = of('Запит 1').pipe(delay(1000)); // Імітація запиту з затримкою 1 сек
+  observable2 = of('Запит 2').pipe(delay(500)); // Імітація запиту з затримкою 0.5 сек
+  observable3 = of('Запит 3').pipe(delay(2000)); // Імітація запиту з затримкою 2 сек
+
+  // Використання forkJoin для комбінування результатів усіх трьох запитів
+  combined = forkJoin([this.observable1, this.observable2, this.observable3]);
 
   ngOnInit(): void {
+    // ************ Використання операторів map, filter, tap *********
+    // this.behaviorSubject
+    //   .pipe(
+    // Використання tap для ведення журналу значень (для дебагінга чи відстеження)
+    // tap((value) => console.log('перед обробкою', value)),
+    // фільтруємо, залишаємо лише значення більші за 0
+    // filter((value) => value > 0),
+    // мапінг: множимо кожне значення на 10
+    // map((value) => value * 10),
+    // )
+    // .subscribe({
+    //   next: (value) => console.log('Оброблене значення:', value),
+    //   complete: () => console.log('Завершено'),
+    // });
+
+    // Випуск значень через behaviorSubject
+    // this.behaviorSubject.next(1);
+    // this.behaviorSubject.next(2);
+    // this.behaviorSubject.next(-1); //це значення ігнорується фільтром
+    // this.behaviorSubject.next(3);
+
+    // Завершення потоку
+    // this.behaviorSubject.complete();
+
     // Subject продукує перше значення
     // this.subjectFirst.next(1);
     //  this.observable.next(); - тут не можемо викликати next
@@ -90,16 +127,24 @@ export class Angular implements OnInit {
     //  ********************AsyncSubject ************
 
     // Випуск значень
-    this.asyncSubject.next(1);
-    this.asyncSubject.next(2);
-    this.asyncSubject.next(3);
+    // this.asyncSubject.next(1);
+    // this.asyncSubject.next(2);
+    // this.asyncSubject.next(3);
 
     // Підписка на AsyncSubject поки що не отримає жодниз значень
-    this.asyncSubject.subscribe((value) => console.log('Підписник на AsyncSubject:', value));
+    // this.asyncSubject.subscribe((value) => console.log('Підписник на AsyncSubject:', value));
 
     // випуск кінцевого значення і завершення
-    this.asyncSubject.next(4);
-    this.asyncSubject.complete();
+    //   this.asyncSubject.next(4);
+    //   this.asyncSubject.complete();
+
+    // використання оператора forkJoin
+    this.combined.subscribe({
+      next: ([response1, response2, response3]) => {
+        console.log('Результат forkJoin:', response1, response2, response3);
+      },
+      complete: () => console.log('Всі запити завершено'),
+    });
   }
 
   // *********************
